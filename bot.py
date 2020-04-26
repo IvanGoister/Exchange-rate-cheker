195461a89d1c6b1aa264df8eb904c832cb201e7f
IvanGoister <47723470+IvanGoister@users.noreply.github.com>
2020-04-26 22:13:14 +0300

    Update bot.py
    Add: 
    1. def.Keyboard()
    2. New buttons
    This code changed on GitHub and need check his working on pc.

--- bot.py
+++ bot.py
@@ -5,31 +5,32 @@ from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
+buttons = ('CreditDnipro', 'PrivatBank', 'HBU')
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
     sti = open('Hello.tgs', 'rb')
     bot.send_sticker(message.chat.id, sti)
 
     # keyboard
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("КредитДніпро")
-    item2 = types.KeyboardButton("Button 2")
-
-    markup.add(item1, item2)
-
+    def Keyboard():
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for button in buttons:
+            item=types.KeyboardButton(button)
+            markup.add(item)
+        return(markup())
+    Keyboard()
     bot.send_message(message.chat.id, "Тестовий бот перевірки курсу валют".format(message.from_user, bot.get_me()),parse_mode='html', reply_markup=markup)
-
-  #  bot.send_message(message.chat.id, "Привіт {0.first_name}.  Вітаю в тестовому боті.".format(message.from_user, bot.get_me()),parse_mode='html', reply_markup=markup)
-
-
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     if message.chat.type == 'private':
-        if message.text == 'КредитДніпро':
+        if message.text == 'CreditDnipro':
             bot.send_message(message.chat.id, str(CD_message))
-        elif message.text == 'Button 2':
-            bot.send_message(message.chat.id, str('Button 2 working'))
+        elif message.text == 'PrivatBank':
+            bot.send_message(message.chat.id, str('PrivatBank no data'))
+        elif message.text == 'NBU':
+            bot.send_message(message.chat.id, str('NBU no data'))
         else:
             bot.send_message(message.chat.id, message.text)
 
