6db6231462235f43dcaed67ea723c91b3e866527
IvanGoister <47723470+IvanGoister@users.noreply.github.com>
2020-04-30 01:40:03 +0300

    Remove parser from file to def
    Remove parser in bot,py and create def for this parser

--- bot.py
+++ bot.py
@@ -1,8 +1,30 @@
 import telebot
 import config
-from parser import CD_message
+
 from telebot import types
 
+def CD_parser():
+    import requests
+    from bs4 import BeautifulSoup as BS
+
+    R_CreditDnipro = requests.get("https://creditdnepr.com.ua/currency")
+    html = BS(R_CreditDnipro.content, 'html.parser')
+    BigBlock = html.find('div', class_='table-overlay')
+    SmallBlock = BigBlock.find('tr', class_='even')
+    items = SmallBlock.find_all('td', style='text-align: center')
+    buy = items[1]
+    sell = items[2]
+    print('КредитДніпро')
+    print('Купівля = ', buy.get_text())
+    print('Продаж = ', sell.get_text())
+    CD_buy = buy.get_text()
+    CD_sell = sell.get_text()
+
+    CD_message = 'Купівля = ' + CD_buy + ', Продаж = ' + CD_sell
+    # R_PUMB = requests.get(" ")
+    # R_Privat = requests.get(" ")
+    return(CD_message)
+CD_message=CD_parser()
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
